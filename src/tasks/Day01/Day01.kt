c07619c9b182efce90e568873294160efec51c6b
Niforances <niforances@gmail.com>
2021-12-02 20:53:58 +0900

    [Create] init code for Day01

--- src/tasks/Day01/Day01.kt
+++ src/tasks/Day01/Day01.kt
@@ -1,20 +1,35 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
+        var count = 0
+        var before = input[0].toInt()
+        var after: Int
 
-
-
-        for (i in input){
-            println(i)
+        for (i in 1 until input.size) {
+            after = input[i].toInt()
+            if (before < after) count++
+            before = after
         }
 
-        return input.size
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var before = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        var after: Int
+
+        for (i in 2 until input.size - 1) {
+            after = input[i - 1].toInt() + input[i].toInt() + input[i + 1].toInt()
+            if (before < after) count++
+            before = after
+        }
+
+        return count
     }
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    println("p1 result: ${part1(input)}")
+    println("p2 result: ${part2(input)}")
 }
